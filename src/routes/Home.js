8b10ce66385c50b341f9cddfde7c9bc2e88183cd
Jimmi James <133916219+jimmijameslinux@users.noreply.github.com>
2024-06-18 20:19:36 +0000

    Fetch functionality added

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,32 @@
+const FetchWeatherData = () => {
+  // URL of the resource you want to fetch
+  const url = `http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_API_KEY}&q=London&aqi=no`;
+
+  // Use the fetch function to make the request
+  fetch(url)
+    .then((response) => {
+      // Check if the request was successful
+      if (!response.ok) {
+        throw new Error("Network response was not ok " + response.statusText);
+      }
+      // Parse the JSON from the response
+      return response.json();
+    })
+    .then((data) => {
+      // Use the data from the response
+      console.log(data);
+    })
+    .catch((error) => {
+      // Handle any errors that occurred during the fetch
+      console.error("There was a problem with the fetch operation:", error);
+    });
+};
+
 export default function Home() {
   return (
     <>
       <div className="App">Home</div>
+      <button onClick={() => FetchWeatherData()}>Fetch</button>
     </>
   );
 }
