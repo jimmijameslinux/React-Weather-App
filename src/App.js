eb19e9cbdd056e81494f4d92c0c63e8c38f14136
Jimmi James <133916219+jimmijameslinux@users.noreply.github.com>
2024-06-18 20:03:53 +0000

    Added components and routes folder containing Navbar and Home js file respectively

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./styles.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./routes/Home";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
